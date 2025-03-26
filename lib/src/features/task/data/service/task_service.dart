3ca4324e36786c336a3a9c52abff906a20d46419
william <williamcawi@gmail.com>
2025-03-26 08:03:50 -0300

    criado o taskService e sua implementação, com seus métodos construídos

---
+++ lib/src/features/task/data/service/task_service.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_playground/src/features/task/data/model/task_model.dart';
+
+abstract interface class TaskService {
+  Future<List<TaskModel>> getTasks();
+  Future<List<TaskModel>> createTask({required TaskModel task});
+  Future<List<TaskModel>> updateTask({required TaskModel task});
+  Future<List<TaskModel>> deleteTask({required int id});
+}
+
+class TaskServiceImpl implements TaskService {
+  List<TaskModel> taskList = [];
+
+  @override
+  Future<List<TaskModel>> createTask({required TaskModel task}) async {
+    taskList.add(task);
+    return Future.value(taskList);
+  }
+
+  @override
+  Future<List<TaskModel>> deleteTask({required int id}) async {
+    taskList.removeWhere((task) => task.id == id);
+    return Future.value(taskList);
+  }
+
+  @override
+  Future<List<TaskModel>> getTasks() async {
+    return Future.value(taskList);
+  }
+
+  @override
+  Future<List<TaskModel>> updateTask({required TaskModel task}) {
+    final index = taskList.indexWhere(
+      (t) => t.id == task.id,
+    );
+    if (index != -1) {
+      taskList[index] = task;
+    }
+
+    return Future.value(taskList);
+  }
+}
