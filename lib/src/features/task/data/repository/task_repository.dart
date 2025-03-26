697d0b67b4e0ff3bccc967320921351d4755e1c9
william <williamcawi@gmail.com>
2025-03-26 08:10:56 -0300

    criado o TaskRepository com a sua implementação e seus métodos construídos.

---
+++ lib/src/features/task/data/repository/task_repository.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_playground/src/features/task/data/model/task_model.dart';
+import 'package:flutter_playground/src/features/task/data/service/task_service.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class TaskRepository {
+  Future<Either<String, List<TaskModel>>> getTasks();
+  Future<Either<String, List<TaskModel>>> createTasks(
+      {required TaskModel task});
+  Future<Either<String, List<TaskModel>>> updateTasks(
+      {required TaskModel task});
+  Future<Either<String, List<TaskModel>>> deleteTasks({required int id});
+}
+
+class TaskRepositoryImpl implements TaskRepository {
+  TaskRepositoryImpl({required TaskService taskService})
+      : _taskService = taskService;
+
+  final TaskService _taskService;
+
+  @override
+  Future<Either<String, List<TaskModel>>> createTasks(
+      {required TaskModel task}) async {
+    try {
+      return Right(await _taskService.createTask(task: task));
+    } catch (error) {
+      return Left(error.toString());
+    }
+  }
+
+  @override
+  Future<Either<String, List<TaskModel>>> deleteTasks({required int id}) async {
+    try {
+      return Right(await _taskService.deleteTask(id: id));
+    } catch (error) {
+      return Left(error.toString());
+    }
+  }
+
+  @override
+  Future<Either<String, List<TaskModel>>> getTasks() async {
+    try {
+      return Right(await _taskService.getTasks());
+    } catch (error) {
+      return Left(error.toString());
+    }
+  }
+
+  @override
+  Future<Either<String, List<TaskModel>>> updateTasks(
+      {required TaskModel task}) async {
+    try {
+      return Right(await _taskService.updateTask(task: task));
+    } catch (error) {
+      return Left(error.toString());
+    }
+  }
+}
